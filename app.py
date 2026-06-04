c9d22118cb2db51480d998afc765092ae39771b0
Purva Salvi <purvasalvi.29@gmail.com>
2026-06-05 01:35:14 +0530

    ...///

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return "Backend running"
 @app.route("/api")
 def home():
     return jsonify({"message": "Smart Fuzzy Irrigation API is running!"})
